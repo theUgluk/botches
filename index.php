7c776a9f51eacc823d871f975c77b3748087cd84
jmuilwijk <muilwijk@cardsolutions.nl>
2020-07-23 14:16:48 +0200

    Adding possibilites of changing names and settings descipions

--- index.php
+++ index.php
@@ -4,6 +4,24 @@ echo "<ul>";
 foreach($files as $file){
   //If not a hidden file (starting with '.')
   if(strpos($file, ".") !== 0 && $file !== "tools") {
-    echo "<li><a href='/Botches/" . $file . "/'>" . $file . "</a></li>";
+    $name = $file;
+    $description = "";
+    //Check for info.json
+    if(file_exists($file . "/info.json")){
+      $settings = json_decode($file . "/info.json", true);
+      if(is_array($settings)){
+        if(array_key_exists("name", $settings)){
+          $name = $settings['name'];
+        }
+          if(array_key_exists("description", $settings)){
+            $description = $settings['description'];
+          }
+      }
+    }
+    echo "<li><a href='/Botches/" . $file . "/'>" . $name . "</a>";
+    if($description !== ""){
+      echo "(" . $description . ")";
+    }
+    echo "</li>";
   }
 }
