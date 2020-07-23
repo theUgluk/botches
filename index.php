96f4521c3b219f50ecbf1d75648bd79d783fa37c
jmuilwijk <muilwijk@cardsolutions.nl>
2020-07-23 14:46:19 +0200

    Added halfway decent html

--- index.php
+++ index.php
@@ -1,32 +1,44 @@
-<?php
-$homedir = dirname(__FILE__);
-$files = scandir($homedir . "/Botches");
+<!doctype html>
+<html>
+  <head>
+    <title>
+      Ugluk's Botches
+    </title>
+  </head>
+  <body>
+    <ul>
+      <?php
+        $homedir = dirname(__FILE__);
+        $files = scandir($homedir . "/Botches");
 
-//Todo: Create file with constants for stuff like homedir, botchdir, tooldir, etc
+        //Todo: Create file with constants for stuff like homedir, botchdir, tooldir, etc
 
-echo "<ul>";
-foreach($files as $file){
-  //If not a hidden file (starting with '.')
-  if(strpos($file, ".") !== 0 && $file !== "tools") {
-    $name = $file;
-    $description = "";
-    //Check for info.json
-    $infoFile = $homedir . "/Botches/" . $file . "/info.json";
-    if(file_exists($infoFile)){
-      $settings = json_decode(file_get_contents($homedir . "/Botches/" . $file . "/info.json"), true);
-      if(is_array($settings)){
-        if(array_key_exists("name", $settings)){
-          $name = $settings['name'];
-        }
-          if(array_key_exists("description", $settings)){
-            $description = $settings['description'];
+        foreach($files as $file){
+          //If not a hidden file (starting with '.')
+          if(strpos($file, ".") !== 0 && $file !== "tools") {
+            $name = $file;
+            $description = "";
+            //Check for info.json
+            $infoFile = $homedir . "/Botches/" . $file . "/info.json";
+            if(file_exists($infoFile)){
+              $settings = json_decode(file_get_contents($homedir . "/Botches/" . $file . "/info.json"), true);
+              if(is_array($settings)){
+                if(array_key_exists("name", $settings)){
+                  $name = $settings['name'];
+                }
+                  if(array_key_exists("description", $settings)){
+                    $description = $settings['description'];
+                  }
+              }
+            }
+            echo "<li><a href='/Botches/" . $file . "/'>" . $name . "</a>";
+            if($description !== ""){
+              echo " (" . $description . ")";
+            }
+            echo "</li>";
           }
-      }
-    }
-    echo "<li><a href='/Botches/" . $file . "/'>" . $name . "</a>";
-    if($description !== ""){
-      echo " (" . $description . ")";
-    }
-    echo "</li>";
-  }
-}
+        }
+      ?>
+    </ul>
+  </body>
+</html>
