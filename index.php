53bc81968ceae07f0c40ea643ca704cfdd6cc689
jmuilwijk <muilwijk@cardsolutions.nl>
2020-07-23 10:17:01 +0200

    Test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set("display_errors", true);
 $files = scandir(dirname(__FILE__) . "/Botches");
 echo "<ul>";
-  var_dump($files)
+var_dump($files);
 foreach($files as $file){
   //If not a hidden file (starting with '.')
   if(strpos($file, "."!== 0) && $file !== "tools") {
