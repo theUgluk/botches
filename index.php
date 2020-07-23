54d0c4139ff73e864a16f611b10d92faec6cf9f4
jmuilwijk <muilwijk@cardsolutions.nl>
2020-07-23 10:16:02 +0200

    Test

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 ini_set("display_errors", true);
 $files = scandir(dirname(__FILE__) . "/Botches");
 echo "<ul>";
-  foreach($files as $file){
-    var_dump($file)
+  var_dump($files)
+foreach($files as $file){
   //If not a hidden file (starting with '.')
   if(strpos($file, "."!== 0) && $file !== "tools") {
     echo "<li><a href='/Botches/" . $file . "/'>" . $file . "</a></li>";
