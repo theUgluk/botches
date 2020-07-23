ae2e89a1c542ff0e1e1b8878a29c9ad469d92d03
jmuilwijk <muilwijk@cardsolutions.nl>
2020-07-23 10:19:39 +0200

    Todo: install a decent linter in Atom

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-ini_set("display_errors", true);
 $files = scandir(dirname(__FILE__) . "/Botches");
 echo "<ul>";
 foreach($files as $file){
-var_dump($file);
   //If not a hidden file (starting with '.')
-  if(strpos($file, "."!== 0) && $file !== "tools") {
+  if(strpos($file, ".") !== 0 && $file !== "tools") {
     echo "<li><a href='/Botches/" . $file . "/'>" . $file . "</a></li>";
   }
 }
