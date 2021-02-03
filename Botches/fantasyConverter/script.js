87b57d2a484ea94a6a942e138dc5e7852a164371
Ugluk <jonasmuilwijk@gmail.com>
2021-02-03 22:03:54 +0100

    adding more options

--- Botches/fantasyConverter/script.js
+++ Botches/fantasyConverter/script.js
@@ -8,18 +8,39 @@ const systems = {
         },
         'centimeter': {
             'value': 100,
-        }
+        },
+        'kilometer': {
+            'value': 0.001,
+        },
     },
     'imperial': {
+        'thou': {
+            value: 39370.1,
+        },
+        'barleycorn': {
+            value: 118.11,
+        },
         'inch': {
             'value': 39.37
         },
-        'feet': {
+        'foot': {
             'value': 3.28084
+        },
+        'yard': {
+            'value': 1.09361
+        },
+        'chain': {
+            'value': 0.0497097
+        },
+        'furlong': {
+            'value': 0.0497097
         },
         'mile': {
             'value': 0.00062137119224,
-        }
+        },
+        'league': {
+            'value': 0.000179986
+        },
     }
 };
 
